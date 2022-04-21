5c4ce68f7fcc5f2ac90b1f50fdb27c34daefcf52
Timurleng44 <muhammedemintanik63@gmail.com>
2022-04-21 18:10:05 +0300

    Add files via upload

--- 6) Consecutive strings/consecutive_strings.c
+++ 6) Consecutive strings/consecutive_strings.c
@@ -2,16 +2,19 @@
 #include <stdlib.h>
 #include <string.h>
 //https://www.codewars.com/kata/56a5d994ac971f1ac500003e/train/c
-char *longestConsec (const char *const strings[/*arr_len*/], int arr_len, int k)
+
+
+
+char *longestConsec (const char *const strings[], int arr_len, int k)
 {
     if(arr_len == 0 || k>arr_len || k<=0){
-        return NULL;
+        return calloc(1,sizeof(char));
     }
 
     int index = 0;
     int max = -1;
     int temp = 0;
-    for(int i=0;i<arr_len-k;i++){
+    for(int i=0;i<arr_len-k+1;i++){
         temp = strlen(strings[i]);
         for(int i2 = 1;i2<k;i2++){
             temp += strlen(strings[i+i2]);
@@ -21,7 +24,7 @@ char *longestConsec (const char *const strings[/*arr_len*/], int arr_len, int k)
             max = temp;
         }
     }
-    char *concatenated_strings = (char *)calloc(max,sizeof(char));
+    char *concatenated_strings = (char *)calloc(max+1,sizeof(char));
     strcpy(concatenated_strings,strings[index]);
     for(int i = 1;i<k;i++){
         strcat(concatenated_strings,strings[i+index]);
@@ -29,10 +32,36 @@ char *longestConsec (const char *const strings[/*arr_len*/], int arr_len, int k)
     return concatenated_strings;
 }
 
+
+
+
+
 int main(){
+    /*
     const char *const strings[6] ={"tree", "foling", "trashy", "blue", "abcdef", "uvwxyz"};
     char *concatenated_strings = longestConsec(strings,6,2);
     printf("%s\n",concatenated_strings);
     free(concatenated_strings);
+    */
+
+    /*
+    const char *const strings[6] ={"ejjjjmmtthh", "zxxuueeg", "aanlljrrrxx", "dqqqaaabbb", "oocccffuucccjjjkkkjyyyeehh"};
+    char * concatenated_strings = longestConsec(strings,5,1);
+    printf("%s\n",concatenated_strings);
+    free(concatenated_strings);
+    */
+    /*
+    const char *const strings[3] ={"itvayloxrp","wkppqsztdkmvcuwvereiupccauycnjutlv","vweqilsfytihvrzlaodfixoyxvyuyvgpck"};
+    char * concatenated_strings = longestConsec(strings,3,2);
+    printf("%s\n",concatenated_strings);
+    free(concatenated_strings);
+    */
+
+    const char *const strings[3] ={"itvayloxrp","wkppqsztdkmvcuwvereiupccauycnjutlv","vweqilsfytihvrzlaodfixoyxvyuyvgpck"};
+    char * concatenated_strings = longestConsec(NULL,0,1);
+    printf("%s\n",concatenated_strings);
+    free(concatenated_strings);
+
+
     return 0;
 }
